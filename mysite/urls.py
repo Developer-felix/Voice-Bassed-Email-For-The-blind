6bf0a2c51f04f11c90ddb7fc75ca38c3b906ab16
Onjomba Felix <73028636+Developer-Felix@users.noreply.github.com>
2021-02-26 12:07:29 -0800

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
-
+admin.site.site_header="Voice bassed Email"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('homepage.urls')),
